0de8bda68367ccaf67f80fe72e71db547dc9ee87
Vitali Zhybuliou <zhybuliou.v.v@gmail.com>
2024-08-31 13:07:52 +0300

    feat: change logo size

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <div className="header-wrapper">
       <div className="header-content">
-        <img src={ImageLogo} alt="Logo" width="100px" height="80px" />
+        <img src={ImageLogo} alt="Logo" width="100px" height="75px" />
       </div>
     </div>
   );
