2582ae07666bf60ede8d368e48ec8c7dda7ec0ba
Vitali Zhybuliou <zhybuliou.v.v@gmail.com>
2024-09-02 18:29:37 +0300

    feat: add page 404

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.scss';
 import Header from './components/Header';
 import Home from './pages/Home';
+import Page404 from './pages/404';
 
 export default function App() {
   ReactGA.initialize('G-YE91DTF7N0');
@@ -11,7 +12,7 @@ export default function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="*" element={<Home />} />
+        <Route path="*" element={<Page404 />} />
       </Routes>
     </>
   );
