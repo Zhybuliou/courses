59288bb8433a91947bb387ac86715c8f8a72c09a
Vitali Zhybuliou <zhybuliou.v.v@gmail.com>
2024-09-02 12:30:35 +0300

    feat: add google analitycs

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import { PureComponent } from 'react';
+import ReactGA from 'react-ga4';
 import { Route, Routes } from 'react-router-dom';
 import './App.scss';
 import Header from './components/Header';
 import Home from './pages/Home';
 
-export default class App extends PureComponent {
-  render() {
-    return (
-      <>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="*" element={<Home />} />
-        </Routes>
-      </>
-    );
-  }
+export default function App() {
+  ReactGA.initialize('G-YE91DTF7N0');
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </>
+  );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,14 @@
+import ReactGA from 'react-ga4';
 import Main from '../components/Main';
 import Mentors from '../components/mentors/Mentors';
 import Schedule from '../components/schedule/Schedule';
 
 export default function Home() {
+  ReactGA.send({
+    hitType: 'pageview',
+    page: '/',
+    title: 'Home',
+  });
   return (
     <>
       <Main />
