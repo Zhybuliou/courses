3a53335273556a12146322a52e6dd02fe63392d7
Vitali Zhybuliou <zhybuliou.v.v@gmail.com>
2024-09-03 15:15:10 +0300

    feat: add seo meta

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import ReactGA from 'react-ga4';
 import { Route, Routes } from 'react-router-dom';
 import './App.scss';
+import { HelmetProvider } from 'react-helmet-async';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Page404 from './pages/404';
@@ -8,12 +9,12 @@ import Page404 from './pages/404';
 export default function App() {
   ReactGA.initialize('G-YE91DTF7N0');
   return (
-    <>
+    <HelmetProvider>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<Page404 />} />
       </Routes>
-    </>
+    </HelmetProvider>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,8 @@ import ReactGA from 'react-ga4';
 import Main from '../components/Main';
 import Mentors from '../components/mentors/Mentors';
 import Schedule from '../components/schedule/Schedule';
+import MetaTags from '../components/meta-tag/metaTag';
+import Logo from '../assets/logo-solo.png';
 
 export default function Home() {
   ReactGA.send({
@@ -11,6 +13,12 @@ export default function Home() {
   });
   return (
     <>
+      <MetaTags
+        title="Обучение по программе: «Налоговый специалист в США»."
+        description="👨‍🎓 Спикеры курса - 5 лицензированных cпециалистов. 🤑 Скидки при раннем бронировании. Длительность - 18 занятий. 💰 Стоимость курса 1700$."
+        image={Logo}
+        name="Налоги в сша"
+      />
       <Main />
       <Mentors />
       <Schedule />
