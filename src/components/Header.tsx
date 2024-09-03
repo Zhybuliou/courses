3b66677920853f8800fb81424ae5720deb517e21
Vitali Zhybuliou <zhybuliou.v.v@gmail.com>
2024-09-03 16:20:02 +0300

    feat: add lasy loading

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,14 @@ export default function Header() {
   return (
     <div className="header-wrapper">
       <div className="header-content">
-        <img src={ImageLogo} alt="Logo" width="100px" height="83px" title="logo usa taxes" />
+        <img
+          src={ImageLogo}
+          alt="Logo"
+          width="100px"
+          height="83px"
+          title="logo usa taxes"
+          loading="lazy"
+        />
       </div>
     </div>
   );
