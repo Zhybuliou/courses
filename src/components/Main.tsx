ed997f7607774d8e16096863af13baa0aae6a1db
Vitali Zhybuliou <zhybuliou.v.v@gmail.com>
2024-09-04 21:03:56 +0300

    feat: </AnimatePresence>

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,4 +1,4 @@
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 import {
   FcGraduationCap,
   FcPlanner,
@@ -49,125 +49,127 @@ const buttonAnimation = {
 
 export default function Main() {
   return (
-    <div className="main-wrapper">
-      <div className="main-content">
-        <div className="main-text">
-          <div className="main-wrapper-blocks">
-            <div className="main-blocks-section">
-              <motion.h1
-                custom={1}
-                variants={textAnimation}
-                initial="hidden"
-                whileInView="visible"
-                viewport={{ once: true }}
-              >
-                Обучение по программе: «Налоговый специалист в США».
-              </motion.h1>
-              <div className="main-blocks">
-                <motion.div
+    <AnimatePresence>
+      <div className="main-wrapper">
+        <div className="main-content">
+          <div className="main-text">
+            <div className="main-wrapper-blocks">
+              <div className="main-blocks-section">
+                <motion.h1
                   custom={1}
-                  variants={blockAnimation}
+                  variants={textAnimation}
                   initial="hidden"
                   whileInView="visible"
-                  className="main-block"
                   viewport={{ once: true }}
                 >
-                  <FcGraduationCap size="80px" />
-                  <div className="main-block-content">
-                    <p>Спикеры курса - 5 лицензированных cпециалистов.</p>
-                  </div>
-                </motion.div>
+                  Обучение по программе: «Налоговый специалист в США».
+                </motion.h1>
+                <div className="main-blocks">
+                  <motion.div
+                    custom={1}
+                    variants={blockAnimation}
+                    initial="hidden"
+                    whileInView="visible"
+                    className="main-block"
+                    viewport={{ once: true }}
+                  >
+                    <FcGraduationCap size="80px" />
+                    <div className="main-block-content">
+                      <p>Спикеры курса - 5 лицензированных cпециалистов.</p>
+                    </div>
+                  </motion.div>
+                  <motion.div
+                    custom={1.5}
+                    variants={blockAnimation}
+                    initial="hidden"
+                    whileInView="visible"
+                    className="main-block"
+                    viewport={{ once: true }}
+                  >
+                    <FcPlanner size={80} />
+                    <div className="main-block-content">
+                      <p>
+                        Длительность - 18 занятий. Начало 29 октября в 12.00 EST(Нью-Йорк, Майами).
+                      </p>
+                    </div>
+                  </motion.div>
+                  <motion.div
+                    custom={2}
+                    variants={blockAnimation}
+                    initial="hidden"
+                    whileInView="visible"
+                    className="main-block"
+                    viewport={{ once: true }}
+                  >
+                    <FcSalesPerformance size="60px" />
+                    <div className="main-block-content">
+                      <p>Скидки при раннем бронировании.</p>
+                    </div>
+                  </motion.div>
+                  <motion.div
+                    custom={2.5}
+                    variants={blockAnimation}
+                    initial="hidden"
+                    whileInView="visible"
+                    className="main-block"
+                    viewport={{ once: true }}
+                  >
+                    <FcClock size="80px" />
+                    <div className="main-block-content">
+                      <p>Длительность каждого урока 3-3,5 часа (с перерывом)</p>
+                    </div>
+                  </motion.div>
+                  <motion.div
+                    custom={3}
+                    variants={blockAnimation}
+                    initial="hidden"
+                    whileInView="visible"
+                    className="main-block"
+                    viewport={{ once: true }}
+                  >
+                    <FcMoneyTransfer size="50px" />
+                    <div className="main-block-content">
+                      <p>Стоимость курса 1700$.</p>
+                    </div>
+                  </motion.div>
+                  <motion.div
+                    custom={3.5}
+                    variants={blockAnimation}
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true }}
+                    className="main-block"
+                  >
+                    <FcCamcorderPro size="60px" />
+                    <div className="main-block-content">
+                      <p>Запись каждого урока остается для просмотра.</p>
+                    </div>
+                  </motion.div>
+                </div>
                 <motion.div
-                  custom={1.5}
-                  variants={blockAnimation}
-                  initial="hidden"
-                  whileInView="visible"
-                  className="main-block"
-                  viewport={{ once: true }}
-                >
-                  <FcPlanner size={80} />
-                  <div className="main-block-content">
-                    <p>
-                      Длительность - 18 занятий. Начало 29 октября в 12.00 EST(Нью-Йорк, Майами).
-                    </p>
-                  </div>
-                </motion.div>
-                <motion.div
-                  custom={2}
-                  variants={blockAnimation}
-                  initial="hidden"
-                  whileInView="visible"
-                  className="main-block"
-                  viewport={{ once: true }}
-                >
-                  <FcSalesPerformance size="60px" />
-                  <div className="main-block-content">
-                    <p>Скидки при раннем бронировании.</p>
-                  </div>
-                </motion.div>
-                <motion.div
-                  custom={2.5}
-                  variants={blockAnimation}
-                  initial="hidden"
-                  whileInView="visible"
-                  className="main-block"
-                  viewport={{ once: true }}
-                >
-                  <FcClock size="80px" />
-                  <div className="main-block-content">
-                    <p>Длительность каждого урока 3-3,5 часа (с перерывом)</p>
-                  </div>
-                </motion.div>
-                <motion.div
-                  custom={3}
-                  variants={blockAnimation}
-                  initial="hidden"
-                  whileInView="visible"
-                  className="main-block"
-                  viewport={{ once: true }}
-                >
-                  <FcMoneyTransfer size="50px" />
-                  <div className="main-block-content">
-                    <p>Стоимость курса 1700$.</p>
-                  </div>
-                </motion.div>
-                <motion.div
-                  custom={3.5}
-                  variants={blockAnimation}
+                  custom={4}
+                  variants={buttonAnimation}
                   initial="hidden"
                   whileInView="visible"
                   viewport={{ once: true }}
-                  className="main-block"
                 >
-                  <FcCamcorderPro size="60px" />
-                  <div className="main-block-content">
-                    <p>Запись каждого урока остается для просмотра.</p>
-                  </div>
+                  <CallButton />
+                  <p className="text-payment">
+                    Payment for the provider service is non-refundable. If you cancel service more
+                    than 21 days before course start , the payment will be fully refunded.
+                  </p>
                 </motion.div>
               </div>
-              <motion.div
-                custom={4}
-                variants={buttonAnimation}
-                initial="hidden"
-                whileInView="visible"
-                viewport={{ once: true }}
-              >
-                <CallButton />
-                <p className="text-payment">
-                  Payment for the provider service is non-refundable. If you cancel service more
-                  than 21 days before course start , the payment will be fully refunded.
-                </p>
-              </motion.div>
-            </div>
-            <div className="main-image-block">
-              <ImageCard />
+              <div className="main-image-block">
+                <ImageCard />
+              </div>
             </div>
           </div>
+          <FlipCountdown />
+          <CountUsers />
+          <CarouselFeedback />
         </div>
-        <FlipCountdown />
-        <CountUsers />
-        <CarouselFeedback />
       </div>
-    </div>
+    </AnimatePresence>
   );
 }
